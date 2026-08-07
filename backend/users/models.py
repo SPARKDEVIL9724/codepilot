81710af9e84c457b7c2984cd4c5204eaa500dc87
SPARKDEVIL9724 <rishi8240agarwal@gmail.com>
2026-08-08 01:44:48 +0530

    Implement Custom User Model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    github_username = models.CharField(max_length=50, blank=True)
+    tech_stack = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.username
